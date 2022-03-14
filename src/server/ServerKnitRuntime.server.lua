2034330748aa54a3a28c696fd4e922dae628986d
Coltrane Willsey <cwillsey06@gmail.com>
2022-03-13 20:18:48 -0600

    Add component support

--- src/server/ServerKnitRuntime.server.lua
+++ src/server/ServerKnitRuntime.server.lua
@@ -1,6 +1,19 @@
 local common = game:GetService("ReplicatedStorage").common
 local Packages = common.Packages
+local Component = require(Packages.component)
 local Knit = require(Packages.knit)
 
+function loadComponents(src: Instance)
+    for _, component in ipairs(src:GetChildren()) do
+        if component:IsA("ModuleScript") then
+            Component.new(require(component))
+        end
+    end
+end
+
 Knit.AddServices(script.Parent.Services)
-Knit:Start():catch(warn)
+Knit:Start()
+:andThen(function()
+    loadComponents(script.Parent.Components)
+end)
+:catch(warn)
