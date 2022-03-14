4f53275a9a767fd09c64e967128bc3587e4b7b53
Coltrane Willsey <cwillsey06@gmail.com>
2022-03-13 21:50:44 -0600

    Add to runtime

--- src/server/ServerKnitRuntime.server.lua
+++ src/server/ServerKnitRuntime.server.lua
@@ -1,6 +1,19 @@
 local common = game:GetService("ReplicatedStorage").common
 local Packages = common.Packages
+local Component = require(Packages.component)
 local Knit = require(Packages.knit)
 
+function loadModules(src: Instance)
+    for _, module in ipairs(src:GetChildren()) do
+        if module:IsA("ModuleScript") then
+            require(module)
+        end
+    end
+end
+
 Knit.AddServices(script.Parent.Services)
-Knit:Start():catch(warn)
+Knit:Start()
+:andThen(function()
+    loadModules(script.Parent.Components)
+end)
+:catch(warn)
