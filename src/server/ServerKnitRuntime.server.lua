c94454f98df85f943898957519759b6f81193d98
Coltrane Willsey <cwillsey06@gmail.com>
2022-03-13 21:36:01 -0600

    Use self-instantiated components

--- src/server/ServerKnitRuntime.server.lua
+++ src/server/ServerKnitRuntime.server.lua
@@ -1,19 +1,6 @@
 local common = game:GetService("ReplicatedStorage").common
 local Packages = common.Packages
-local Component = require(Packages.component)
 local Knit = require(Packages.knit)
 
-function loadComponents(src: Instance)
-    for _, component in ipairs(src:GetChildren()) do
-        if component:IsA("ModuleScript") then
-            Component.new(require(component))
-        end
-    end
-end
-
 Knit.AddServices(script.Parent.Services)
-Knit:Start()
-:andThen(function()
-    loadComponents(script.Parent.Components)
-end)
-:catch(warn)
+Knit:Start():catch(warn)
